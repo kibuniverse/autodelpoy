cb74cbd0075634e32ec74eebbe8374daaa73f120
kibuniverse <3050540005@qq.com>
2022-07-02 22:12:16 +0800

    per nginx etc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import { execSync } from "child_process";
-import { existsSync } from "fs";
+import { existsSync, writeFileSync } from "fs";
 import { generateKey } from "./utils";
 import express from "express";
 const app = express();
 const userName = "kibuniverse";
 const repoName = "test-autodeploy";
 
+const commitHash = "f95a7c3";
+
 const clonePath = `git@github.com:${userName}/${repoName}.git`;
-const commitHash = "ad246b838a30f70a351779a677352ad6968e3da8";
 const key = generateKey({ userName, repoName, commitHash });
-app.use(express.static(commitHash));
+
 if (!existsSync(repoName)) {
   execSync(`git clone ${clonePath}`);
 }
@@ -20,8 +21,16 @@ execSync(
   }
 );
 console.log("build success");
+// move dist dir
 execSync(`cd ${repoName} && cp -r dist/ /var/www/${key}`);
-
+// add nginx conf
+const nginxConf = `location /${key} {  
+  alias /var/www/${key}/;
+}`;
+writeFileSync(`/etc/nginx/locations/${key}.conf`, nginxConf);
+// restart nginx
+console.log("key", key);
+execSync("nginx -s reload");
 app.listen(3002, () => {
   console.log("启动在3000端口");
 });
